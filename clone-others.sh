73e1b736fb4d9a284c5a7d405e7c3b1211c627f9
SuperAviation001 <83483383+SuperAviation001@users.noreply.github.com>
2025-04-19 15:22:37 +1000

    tetris: Clone hardware/mediatek along with other sources

--- clone-others.sh
+++ clone-others.sh
@@ -6,12 +6,15 @@ cd ../../../
 rm -rf vendor/nothing
 rm -rf kernel/nothing
 rm -rf device/nothing/tetris-kernel
+rm -rf hardware/mediatek
 
 git clone https://github.com/NothingOSS/android_kernel_6.1_nothing_mt6878 kernel/nothing/tetris
 
-git clone https://gitea.com/SuperAviation001/android_vendor_nothing_tetris/src/branch/15_v2/ vendor/nothing/tetris
+git clone https://gitlab.com/SuperAviation001/android_vendor_nothing_tetris/ vendor/nothing/tetris
 
 git clone https://github.com/SuperAviation001/android_device_nothing_tetris-kernel device/nothing/tetris-kernel
+
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-22.2 hardware/mediatek
 
 cd device/nothing/tetris
 
