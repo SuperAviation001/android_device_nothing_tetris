96f785bbe397dabff9fd65f60d75af1da0752860
luk1337 <priv.luk@gmail.com>
2025-04-23 12:34:47 +1000

    tetris: overlay: Add reboot_fastboot action to power menu
    Signed-off-by: SuperAviation001 <superaviation001@gmail.com>

--- device.mk
+++ device.mk
@@ -67,6 +67,10 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
+
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage
+
 PRODUCT_PACKAGES += \
     FrameworksResOverlayTetris \
     SettingsResTetris \
