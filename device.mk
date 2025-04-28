a7f85a0b15d5882ec7f491d184867ccc806ef2d2
SuperAviation001 <superaviation001@gmail.com>
2025-04-28 19:58:51 +1000

    tetris: Build android.hardware.power-service-mediatek from source
    Change-Id: I4427349da209b27052b35242d7c9da6f086d62bb
    Signed-off-by: SuperAviation001 <superaviation001@gmail.com>

--- device.mk
+++ device.mk
@@ -231,6 +231,7 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     vendor.mediatek.hardware.mtkpower@1.2-service.stub \
+    android.hardware.power-service-mediatek \
     android.hardware.power-V4-ndk.vendor \
     android.hardware.power@1.2.vendor
 
