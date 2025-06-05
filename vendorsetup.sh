5b851a4193630e9dd5ed9be20cb1203bf5ca2e8a
SuperAviation001 <superaviation001@gmail.com>
2025-06-05 19:29:30 +1000

    Revert "tetris: Switch to kernel headers"
    This reverts commit 8b2e8f66d88e96c4e5b8290a322f2e324498e09d.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,8 +8,11 @@ fi
 if [ ! -d "vendor/nothing/tetris" ]; then
   git clone https://gitlab.com/SuperAviation001/android_vendor_nothing_tetris -b a15 vendor/nothing/tetris
 fi
+if [ ! -d "kernel/nothing/tetris" ]; then
+  git clone https://github.com/SuperAviation001/android_kernel_nothing_tetris -b lineage-22.2 kernel/nothing/tetris
+fi
 if [ ! -d "device/nothing/tetris-kernel" ]; then
-  git clone https://github.com/SuperAviation001/android_device_nothing_tetris-kernel -b a15 device/nothing/tetris-kernel
+  git clone https://github.com/SuperAviation001/android_device_nothing_tetris-kernel -b lineage-22.2 device/nothing/tetris-kernel
 fi
 if [ ! -d "device/nothing/tetris-kernel" ]; then
   https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
