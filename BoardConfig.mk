da4b386b5830317014c3d142b3093dbc7affdbda
SuperAviation001 <superaviation001@gmail.com>
2025-06-24 15:42:20 +1000

    Reapply "tetris: Use seperate SEPolicy vendor"
    This reverts commit 6ad4b438d46605819cb7a297fc4d664a2cbda248.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,7 +137,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2025-02-08
 
 # SELinux
--include device/mediatek/sepolicy_vndr/SEPolicy.mk
+-include device/nothing/tetris-sepolicy/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
