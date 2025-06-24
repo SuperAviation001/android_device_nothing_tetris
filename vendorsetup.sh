67148f59ce61a9043cfe91b218678ab09b5c3e62
SuperAviation001 <superaviation001@gmail.com>
2025-06-24 15:42:14 +1000

    Revert "tetris: Clone mediatek sepolicy vendor for newer mediatek devices"
    This reverts commit a18afeac50ef18675a7fd6328ab4b34c0bd31884.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,5 +11,5 @@ if [ ! -d "kernel/nothing/mt6878" ]; then
   git clone https://github.com/SuperAviation001/android_kernel_nothing_tetris -b lineage-22.2 kernel/nothing/mt6878
 fi
 if [ ! -d "device/mediatek/sepolicy_vndr" ]; then
-  git clone https://github.com/SuperAviation001/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
+  git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
 fi
