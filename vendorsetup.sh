2285d82c0815ca297086652b9b094c8e83476770
SuperAviation001 <superaviation001@gmail.com>
2025-06-26 21:23:46 +1000

    Revert "tetris: Build kernel from source"
    This reverts commit 5b0e09b9438abc6b4a502ff3beb7fe729ecf6b6a.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,8 +7,11 @@ fi
 if [ ! -d "vendor/nothing/tetris" ]; then
   git clone https://gitlab.com/SuperAviation001/android_vendor_nothing_tetris -b a15 vendor/nothing/tetris
 fi
-if [ ! -d "kernel/nothing/mt6878" ]; then
-  git clone https://github.com/SuperAviation001/android_kernel_nothing_tetris -b lineage-22.2 kernel/nothing/mt6878
+if [ ! -d "kernel/nothing/tetris" ]; then
+  git clone https://github.com/SuperAviation001/android_kernel_nothing_tetris -b lineage-22.2 kernel/nothing/tetris
+fi
+if [ ! -d "device/nothing/tetris-kernel" ]; then
+  git clone https://github.com/SuperAviation001/android_device_nothing_tetris-kernel -b lineage-22.2 device/nothing/tetris-kernel
 fi
 if [ ! -d "device/nothing/tetris-sepolicy" ]; then
   git clone https://github.com/SuperAviation001/android_device_nothing_tetris-sepolicy.git -b lineage-22.2 device/nothing/tetris-sepolicy
