9b57e9533670cd931faa525a2b339cb7e6d72b84
SuperAviation001 <superaviation001@gmail.com>
2025-06-28 12:26:24 +1000

    tetris: SEPolicy: Resolve denials and compilation errors
    * also import external sepolicy into tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -132,7 +132,8 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2025-02-08
 
 # SELinux
--include device/nothing/tetris-sepolicy/SEPolicy.mk
+-include device/nothing/tetris/sepolicy/mtk/SEPolicy.mk
+-include device/nothing/tetris/sepolicy/prop/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
