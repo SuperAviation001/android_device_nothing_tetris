73b58fcc912e3a03d5542410f109ffac432ddd7e
SuperAviation001 <superaviation001@gmail.com>
2025-06-28 12:27:49 +1000

    tetris: Build libnetutils in all variants
    * Fix LINKER failure

--- device.mk
+++ device.mk
@@ -404,6 +404,8 @@ PRODUCT_PACKAGES += \
     libkeystore-wifi-hidl \
     libkeystore-engine-wifi-hidl \
     libwifi-hal-wrapper \
+    libnetutils \
+    libnetutils.vendor \
     wifi_legacy \
     wpa_supplicant
 
