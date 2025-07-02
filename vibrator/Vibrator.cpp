24f1308ecf782749d507ff053a65748eac4cb708
SuperAviation001 <superaviation001@gmail.com>
2025-07-02 14:31:56 +1000

    tetris: vibrator: fixup compilation errors and set boot prop

--- vibrator/Vibrator.cpp
+++ vibrator/Vibrator.cpp
@@ -13,9 +13,9 @@
 
 #include "aac_vibra_function.h"
 
-#define RICHTAP_LIGHT_STRENGTH 66
-#define RICHTAP_MEDIUM_STRENGTH 85
-#define RICHTAP_STRONG_STRENGTH 94
+#define RICHTAP_LIGHT_STRENGTH 69
+#define RICHTAP_MEDIUM_STRENGTH 89
+#define RICHTAP_STRONG_STRENGTH 99
 
 namespace aidl {
 namespace android {
