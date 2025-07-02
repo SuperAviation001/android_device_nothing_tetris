5da9ddeec7245025ebe89ba2f886b9e5a798c5d4
SuperAviation001 <superaviation001@gmail.com>
2025-07-02 14:51:14 +1000

    tetris: vibrator: Play more premium-feeling effects

--- vibrator/Vibrator.cpp
+++ vibrator/Vibrator.cpp
@@ -95,8 +95,8 @@ ndk::ScopedAStatus Vibrator::perform(Effect effect, EffectStrength es,
                                      int32_t* _aidl_return) {
     int32_t strength;
 
-    if (effect == Effect::TICK || effect == Effect::CLICK)
-         effect = Effect::THUD;
+    if (effect == Effect::THUD || effect == Effect::TICK || effect == Effect::POP)
+         effect = Effect::CLICK;
 
     if (effect < Effect::CLICK || effect > Effect::HEAVY_CLICK)
         return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_UNSUPPORTED_OPERATION));
