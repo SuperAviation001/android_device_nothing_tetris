3ce340f30c358033d678a7112f185aff1d63a62a
SuperAviation001 <superaviation001@gmail.com>
2025-08-24 16:12:35 +1000

    tetris: Import lineage 21 extract utils into tree

--- extract-files.sh
+++ extract-files.sh
@@ -16,8 +16,9 @@ MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 ANDROID_ROOT="${MY_DIR}/../../.."
+EXTRACTUTILS_ROOT="${MY_DIR}"
 
-HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+HELPER="${EXTRACTUTILS_ROOT}/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
