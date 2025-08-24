88bf8bc53f547230dbec58762923cc2669d1b403
Bruno Martins <bgcngm@gmail.com>
2025-08-24 16:10:06 +1000

    tetris: Reserve space for GAPPS

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,6 +106,9 @@ TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_COPY_OUT_VENDOR := vendor
 TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 
+# Reserve space for GAPPS
+-include vendor/lineage/config/BoardConfigReservedSize.mk
+
 # Platform
 BOARD_HAS_MTK_HARDWARE := true
 TARGET_BOARD_PLATFORM := mt6878
