c954142bf103b2fb6dabbbe44678e08c19de3018
SuperAviation001 <superaviation001@gmail.com>
2025-08-25 21:44:26 +1000

    tetris: Don't clone kernel
    * we use prebuilt headers

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,9 +9,6 @@ fi
 if [ ! -d "vendor/nothing/tetris" ]; then
   git clone https://gitlab.com/SuperAviation001/android_vendor_nothing_tetris -b a15 vendor/nothing/tetris
 fi
-if [ ! -d "kernel/nothing/tetris" ]; then
-  git clone https://github.com/SuperAviation001/android_kernel_nothing_tetris -b lineage-22.2 kernel/nothing/tetris
-fi
 if [ ! -d "device/nothing/tetris-kernel" ]; then
   git clone https://github.com/SuperAviation001/android_device_nothing_tetris-kernel -b lineage-22.2 device/nothing/tetris-kernel
 fi
