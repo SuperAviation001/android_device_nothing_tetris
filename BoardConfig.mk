004a252ada54aba65070863e3a22d2535080d522
SuperAviation001 <superaviation001@gmail.com>
2025-08-26 20:04:56 +1000

    tetris: sepolicy: include sepolicy/mtk before sepolicy/prop
    * sepolicy/prop being included before was causing errors that we already fixed to come back up again

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -132,8 +132,9 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2025-02-08
 
 # SELinux
--include device/nothing/tetris/sepolicy/prop/SEPolicy.mk
 -include device/nothing/tetris/sepolicy/mtk/SEPolicy.mk
+-include device/nothing/tetris/sepolicy/prop/SEPolicy.mk
+
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
