85a71ef164ffe380106c6718dc3d9dab7bc38900
SuperAviation001 <superaviation001@gmail.com>
2026-01-31 14:19:48 +1100

    tetris: clone a16 branches

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,8 +7,8 @@ if [ ! -d "hardware/mediatek" ]; then
   git clone https://github.com/SuperAviation001/android_hardware_mediatek -b lineage-23.2 hardware/mediatek
 fi
 if [ ! -d "vendor/nothing/tetris" ]; then
-  git clone https://gitlab.com/SuperAviation001/android_vendor_nothing_tetris -b a15 vendor/nothing/tetris
+  git clone https://gitlab.com/SuperAviation001/android_vendor_nothing_tetris -b a16 vendor/nothing/tetris
 fi
 if [ ! -d "device/nothing/tetris-kernel" ]; then
-  git clone https://github.com/SuperAviation001/android_device_nothing_tetris-kernel -b lineage-22.2 device/nothing/tetris-kernel
+  git clone https://github.com/SuperAviation001/android_device_nothing_tetris-kernel -b lineage-23.2 device/nothing/tetris-kernel
 fi
