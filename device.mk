bccab7c945650d8a594159eee54e6a177ab33529
SuperAviation001 <superaviation001@gmail.com>
2026-02-01 12:14:41 +1100

    tetris: build `android.hardware.sensors-service.multihal` from source

--- device.mk
+++ device.mk
@@ -368,6 +368,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
+    android.hardware.sensors-service.multihal \
     android.hardware.sensors@1.0.vendor \
     android.hardware.sensors@2.1.vendor \
     android.hardware.sensors-V2-ndk.vendor \
