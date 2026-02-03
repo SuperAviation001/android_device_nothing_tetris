e0308d66e3bd1cd5ad3ea43d2685f3d12a0d70d2
SuperAviation001 <superaviation001@gmail.com>
2026-02-03 21:58:12 +1100

    [DNM] Tetris: fix build time analysing errors
    * not finished, taking a lil break rn

--- extract-files.py
+++ extract-files.py
@@ -25,15 +25,16 @@ namespace_imports = [
 ]
 
 
-#def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
-#    return f'{lib}_{partition}' if partition == 'vendor' else None
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
 
 
-#lib_fixups: lib_fixups_user_type = {
-#    **lib_fixups,
-#    (
-#    ): lib_fixup_vendor_suffix,
-#}
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+    'vendor.mediatek.hardware.videotelephony-V1-ndk'
+    ): lib_fixup_vendor_suffix,
+}
 
 
 blob_fixups: blob_fixups_user_type = {
